011ba97634d7ec3641e4183f986523ae32c00561
QIWEI GAO <oohmyv@gmail.com>
2017-08-20 23:19:43 +0800

    update

--- js/MotionDetector/Core.js
+++ js/MotionDetector/Core.js
@@ -40,6 +40,33 @@
 			main();
 		}
 
+		function render() {
+			oldImage = currentImage;
+			currentImage = webCam.captureImage(false);
+
+			if(!oldImage || !currentImage) {
+				return;
+			}
+
+			var vals = imageCompare.compare(currentImage, oldImage, width, height);
+
+			topLeft[0] = vals.topLeft[0] * 10;
+			topLeft[1] = vals.topLeft[1] * 10;
+
+			bottomRight[0] = vals.bottomRight[0] * 10;
+			bottomRight[1] = vals.bottomRight[1] * 10;
+
+			document.getElementById('movement').style.top = topLeft[1] + 'px';
+			document.getElementById('movement').style.left = topLeft[0] + 'px';
+
+			document.getElementById('movement').style.width = (bottomRight[0] - topLeft[0]) + 'px';
+			document.getElementById('movement').style.height = (bottomRight[1] - topLeft[1]) + 'px';
+
+			topLeft = [Infinity,Infinity];
+			bottomRight = [0,0]
+
+		}
+
 
 
 
